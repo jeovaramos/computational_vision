5d54685f13c7e286d2a9856ac928950f087cf5af
jeovaramos <silvajunior.jeova@gmail.com>
2021-09-21 19:57:22 -0300

    POO workflow development and coupling w/ streamlit

--- lib/image_detection.py
+++ lib/image_detection.py
@@ -1,16 +1,15 @@
 import cv2
 import time
 import numpy as np
+import streamlit as st
 import matplotlib.pyplot as plt
 
 
 class ImageDetection:
-    def __init__(self, cap: cv2.VideoCapture):
+    def __init__(self):
         self.class_names, self.colors = self.load_labels()
-        self.net = self.load_model()
 
-        self.image, self.H, self.W = self.webcam_image(cap)
-        # net, layer_outputs, fps = blob_image(net, image)
+        return None
 
     def three_lists_generator(self):
         boxes = []
@@ -34,8 +33,9 @@ class ImageDetection:
         return class_names, colors
 
     def load_model(
-        model_path='models/yolov4.weights',
-        cfg_path='models/cfg/yolov4.cfg'
+        self,
+        model_path='models/yolov4-tiny.weights',
+        cfg_path='models/cfg/yolov4-tiny.cfg'
     ):
         net = cv2.dnn.readNet(
             model=model_path,
@@ -43,36 +43,37 @@ class ImageDetection:
 
         return net
 
-    def show_img(img):
+    def show_img(self, img):
         fig = plt.gcf()
         fig.set_size_inches(18.5, 10.5)
         plt.axis('off')
         plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
         plt.show()
 
-    def reshape_img(img, max_width=600):
-        if img.shape[1] > max_width:
-            img = cv2.resize(
-                img,
+    def reshape_image(self, image, max_width=600):
+        if image.shape[1] > max_width:
+            image = cv2.resize(
+                image,
                 (
                     max_width,
-                    int(img.shape[0] * max_width / img.shape[1]
+                    int(image.shape[0] * max_width / image.shape[1]
                         )
                 )
             )
-        return img
+        return image
 
     def load_image(self, img_path):
         image = cv2.imread(img_path)
-        image = self.reshape_img(image)
+        image = self.reshape_image(image)
         return image, image.shape[:2]
 
     def webcam_image(self, cap: cv2.VideoCapture):
         image = cap.read()[1]
-        image = self.reshape_img(image)
+        # image = self.reshape_image(image)
+
         return image, image.shape[:2]
 
-    def blob_image(net, image):
+    def blob_image(self, net, image):
         start = time.time()
 
         ln = net.getLayerNames()
@@ -90,11 +91,12 @@ class ImageDetection:
         layer_outputs = net.forward(ln)
         end = time.time()
 
-        fps = f'{end - start:.2f}'
+        fps = 1 / (end - start)
 
-        return net, layer_outputs, fps
+        return layer_outputs, fps
 
     def detections(
+        self,
         detection, boxes, confidences,
         class_ids, H, W, threshold=0.5
     ):
@@ -115,40 +117,124 @@ class ImageDetection:
 
         return boxes, confidences, class_ids
 
-    def nms_objects(boxes, confidences, threshold=0.5, threshold_nms=0.3):
+    def nms_objects(
+        self,
+        boxes, confidences,
+        threshold=0.5, threshold_nms=0.3
+    ):
+
         objects = cv2.dnn.NMSBoxes(
             boxes, confidences, threshold, threshold_nms)
         return objects
 
     def draw_bounding_boxes(
+        self,
         objects, image, boxes,
-        confidences, class_ids,
-        class_names, colors
+        confidences, class_ids
     ):
 
         if len(objects) > 0:
             for ii in objects.flatten():
                 (x, y) = (boxes[ii][0], boxes[ii][1])
                 (w, h) = (boxes[ii][2], boxes[ii][3])
-                color = [int(c) for c in colors[class_ids[ii]]]
+                color = [int(c) for c in self.colors[class_ids[ii]]]
+
+                fill_area = np.full(
+                    (image.shape), (0, 0, 0), dtype=np.uint8
+                )
+
+                text = f'{self.class_names[class_ids[ii]]}: '
+                f'{confidences[ii]:.2f}'
+                cv2.putText(
+                    fill_area, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX,
+                    0.5, (255, 255, 255), 2
+                )
+
+                fx, fy, fw, fh = cv2.boundingRect(
+                    fill_area[:, :, 2]
+                )
 
                 cv2.rectangle(image, (x, y), (x+w, y+h), color, 2)
-                text = f'{class_names[class_ids[ii]]}: {confidences[ii]:.2f}'
+                cv2.rectangle(image, (fx, fy), (fx+fw, fy+fh), color, -1)
+                cv2.rectangle(image, (fx, fy), (fx+fw, fy+fh), color, 3)
+
                 cv2.putText(
-                    image, text, (x, y-5),
-                    cv2.FONT_HERSHEY_SIMPLEX,
-                    0.5,
-                    color,
-                    2
+                    image, text, (x, y-5), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
+                    (0, 0, 0), 1
                 )
 
                 # TODO: Save detections for specifc class
                 # detection = image_cp[y:y+h, x:x+w]
 
+                # TODO: Send a text message to the user if
+                # the object is a person or pre defined class
+
             return image
         else:
-            print('No detections')
-            return None
+            # print('No detections')
+            return image
+
+    def show_fps(self, image, fps):
+        cv2.putText(
+            img=image,
+            text=f'{fps:.2f} FPS', org=(20, 30),
+            fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+            fontScale=1,
+            color=(0, 255, 0),
+            thickness=2
+        )
+
+        return image
+
+    def main(self):
+        st.title("Webcam live objects detection")
+        run = st.checkbox('Open Webcam')
+
+        # Webcam
+        FRAME_WINDOW = st.image([])
+        cap = cv2.VideoCapture(0)
+        net = self.load_model()
+
+        threshold = 0.5
+        threshold_nms = 0.3
+
+        while run:
+
+            image, (H, W) = self.webcam_image(cap)
+            layer_outputs, fps = self.blob_image(net, image)
+
+            boxes, confidences, class_ids = self.three_lists_generator()
+            for output in layer_outputs:
+                for detection in output:
+                    boxes, confidences, class_ids = self.detections(
+                        detection,
+                        boxes, confidences, class_ids,
+                        H, W,
+                        threshold=threshold
+                    )
+
+            # Non-maximum suppression
+            objects = self.nms_objects(
+                boxes, confidences,
+                threshold=threshold, threshold_nms=threshold_nms
+            )
+
+            # Draw bounding boxes
+            image = self.draw_bounding_boxes(
+                objects, image,
+                boxes, confidences, class_ids)
+
+            # Show FPS
+            image = self.show_fps(image, fps)
+            # cv2.imshow('image', image)
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            FRAME_WINDOW.image(image)
+
+            if cv2.waitKey(10) & 0xFF == ord('q'):
+                break
+
+        cap.release()
+        cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
